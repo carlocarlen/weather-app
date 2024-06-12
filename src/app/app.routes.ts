f2a54a7a4f3717d4013d3cb038359c529233b390
Carlo Maffi <cmaffi@cross-systems.com>
2024-06-12 09:44:19 +0200

    location component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LocationFormComponent } from './location/location-form/location-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LocationFormComponent}
+];
