25381b0b29803d3ce71facc99fb3caaff4bb7b8a
Carlo Maffi <cmaffi@cross-systems.com>
2024-06-12 11:00:19 +0200

    Form: add model

--- src/app/location/location-form/location-form.component.ts
+++ src/app/location/location-form/location-form.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'wth-location-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './location-form.component.html',
   styleUrl: './location-form.component.css'
 })
 export class LocationFormComponent {
+
+  zipCode: string = "";
+
+  onSubmit() {
+    console.log(`Submitted zip code ${this.zipCode}`);
+  }
 
 }
